ba142c2739b68a058a5cae15867d0bdc6c69eee7
jonathanrestrepo06 <jonathanrestrepo06@gmail.com>
2024-04-30 22:50:16 -0500

    Create funcionality of products for User

---
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.proyecto.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.ecommerce.service.ProductService;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping("")
+	public String home(Model model) {
+		
+		model.addAttribute("products", productService.findAll());
+		return "user/home";
+		
+	}
+
+}
