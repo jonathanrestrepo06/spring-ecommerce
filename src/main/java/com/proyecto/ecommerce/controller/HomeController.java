b5de9997d068cff26681602c60769e94b3884514
jonathanrestrepo06 <jonathanrestrepo06@gmail.com>
2024-05-04 14:27:26 -0500

    Add cuantity and Id to add to ShoppingCart

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.proyecto.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.proyecto.ecommerce.model.DetailOrder;
+import com.proyecto.ecommerce.model.Order;
 import com.proyecto.ecommerce.model.Product;
 import com.proyecto.ecommerce.service.ProductService;
 
@@ -23,6 +28,12 @@ public class HomeController {
 	
 	@Autowired
 	private ProductService productService;
+	
+	//Save details of the order
+	List<DetailOrder> details = new  ArrayList<DetailOrder>();
+	
+	//Dates of the order
+	Order order = new Order();
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -46,7 +57,16 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart() {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cuantity) {
+		
+		DetailOrder detailOrder = new DetailOrder();
+		Product product = new Product();
+		double valueTotal = 0;
+		
+		Optional<Product> optionalProduct = productService.get(id);
+		
+		log.info("Product added: {}",optionalProduct.get());
+		log.info("Cuantity: {}",cuantity);
 		
 		return "user/shoppingCart";
 	}
