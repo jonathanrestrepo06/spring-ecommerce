9262a93a2d63305aea4d094e7d85174f9f132266
jonathanrestrepo06 <jonathanrestrepo06@gmail.com>
2024-05-04 14:14:05 -0500

    Add shoppingCart view

--- src/main/java/com/proyecto/ecommerce/controller/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.proyecto.ecommerce.model.Product;
@@ -42,6 +43,12 @@ public class HomeController {
 		
 		return "user/productHome";
 		
+	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		
+		return "user/shoppingCart";
 	}
 	
 	
