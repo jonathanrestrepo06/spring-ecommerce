465a4650c447b6c4b9e5e995ff490312bc61c0eb
jonathanrestrepo06 <jonathanrestrepo06@gmail.com>
2024-06-05 21:34:04 -0500

    Add number of order

--- src/main/java/com/proyecto/ecommerce/service/IOrderService.java
+++ src/main/java/com/proyecto/ecommerce/service/IOrderService.java
@@ -1,7 +1,10 @@
 package com.proyecto.ecommerce.service;
 
+import java.util.List;
+
 import com.proyecto.ecommerce.model.Order;
 
 public interface IOrderService {
+	List<Order> findAll();
 	Order save(Order order);
 }
--- src/main/java/com/proyecto/ecommerce/service/OrderServiceImpl.java
+++ src/main/java/com/proyecto/ecommerce/service/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package com.proyecto.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,42 @@ public class OrderServiceImpl implements IOrderService{
 	public Order save(Order order) {
 		
 		return orderRepository.save(order);
+	}
+	@Override
+	public List<Order> findAll() {
+		
+		return orderRepository.findAll();
+	}
+	
+	public String generateNumberOrder() {
+		
+		int number = 0;
+		String numberConcat = "";
+		
+		List<Order> orders = findAll();
+		List<Integer> numbers = new ArrayList<>();
+		
+		orders.stream().forEach(o -> numbers.add(Integer.parseInt(o.getNumber())));
+		
+		if(orders.isEmpty()) {
+			number = 1;
+		}else {
+			number = numbers.stream().max(Integer::compare).get();
+			number++;
+		}
+		
+		if(number<10) {
+			numberConcat = "000000000" + String.valueOf(number);
+		}else if (number<100) {
+			numberConcat = "00000000" + String.valueOf(number);
+		}else if (number<1000) {
+		numberConcat = "0000000" + String.valueOf(number);
+	    }else if (number<10000) {
+		numberConcat = "000000" + String.valueOf(number);
+	    }
+		
+		
+		return numberConcat;
 	}
 
 }
